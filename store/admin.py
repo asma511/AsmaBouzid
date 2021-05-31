bdc421b8218d4ccf586eac99c26f78ee49b82881
asma511 <asma.bouzid9862@gmail.com>
2021-06-01 00:09:56 +0100

    first commit

---
+++ store/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from .models import *
+
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
+admin.site.register(sante)
+admin.site.register(visage)
+admin.site.register(corps)
+admin.site.register(cheveux)
